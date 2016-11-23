0e064b13c4aa428908be39a460fecf225bc7a021
shrumit <shrumit.m@hotmail.com>
2016-11-23 13:04:53 -0500

    Add remaining cells feature to ErrorEditDialog

--- src/gui/ErrorEditDialog.h
+++ src/gui/ErrorEditDialog.h
@@ -2,6 +2,7 @@
 #define ERROREDITDIALOG_H
 
 #include <QDialog>
+#include <QTableWidgetItem>
 #include <vector>
 #include <string>
 #include <set>
@@ -29,6 +30,7 @@ private slots:
      void on_cancel_clicked();
      void on_findNext_clicked();
      void on_findPrev_clicked();
+     void on_tableWidget_itemChanged(QTableWidgetItem *item);
 
 private:
     std::vector<std::vector<std::string>*> errorList;
@@ -36,6 +38,7 @@ private:
     std::vector<std::string> mandatoryList;
     std::set<std::tuple<int,int>> error_cells;
     Ui::ErrorEditDialog *ui;
+    bool error_lock = false;
 
     void saveData();
     void nextprevHandler(bool next);
